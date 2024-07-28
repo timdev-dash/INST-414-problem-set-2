f893c1dc4b255997882c2163fb9c1b87bd0587e1
Tim Scott <118086790+timdev-dash@users.noreply.github.com>
2024-07-27 22:46:53 -0400

    ETL portion complete
    Added file_manger and finalized ETL script

---
+++ src/file_manager.py
@@ -0,0 +1,91 @@
+'''
+This script contains the various methods of opening and saving data with files. Its
+intention is to be used as a code package to carry over from project to 
+project without needing to rewrite or copy/paste working ingestion script
+'''
+
+# Standard library imports
+from pathlib import Path
+import requests
+
+# Third party imports
+import pandas as pd
+
+# Setting constants
+MAIN_FOLDER: Path = Path(__file__).absolute().parent
+
+def from_web_b(self, url: str, download_settings: dict, file_name: str, relative_path: str = ''):
+    '''
+    Ingests a targeted file from a website and saves in a directory with a name in a binary form
+    
+    Parameters:
+        url(string): The url to download the file from
+        download_settings(dict): The dictionary of settings to be used in the download
+        file_name(string): The name the file should be saved as, included extension
+        relative_path(string): Details on where, other than the script's native directory
+            the file should be saved
+    '''
+
+    # Requests data from the targeted site
+    response: requests = requests.get(url, params = download_settings, timeout = 90)
+
+    # Saves requested file in the provided directory
+    with (open(MAIN_FOLDER / relative_path / file_name, mode = "wb")) as file:
+        file.write(response.content)
+
+def from_web(url: str, download_settings: dict, file_name: str, relative_path: str = '', encoding_type: str = 'UTF-8'):
+    '''
+    Ingests a targeted file from a website and saves in a directory with a name
+    
+    Parameters:
+        url(string): The url to download the file from
+        download_settings(dict): The dictionary of settings to be used in the download
+        file_name(string): The name the file should be saved as, included extension
+        relative_path(string): Details on where, other than the script's native directory
+            the file should be saved
+        encoding_type(string): The type of encoding to be used for opening the file.
+            Defaults to UTF-8
+    '''
+
+    # Requests data from the targeted site
+    response: requests = requests.get(url, params = download_settings, timeout = 90)
+
+    # Saves requested file in the provided directory
+    with (open(MAIN_FOLDER / relative_path / file_name, mode = "w", encoding = encoding_type)) as file:
+        file.write(response.content)
+
+def df_from_csv(path_and_file: str):
+    '''
+    Ingests a targeted file from a directory and returns it as a dataframe using a generic
+    numeric index and default column names.
+
+    Parameters:
+        path_and_file(string): Location and name details for the csv file to be opened
+
+    Returns:
+        data(Dataframe): The dataframe containing the contents of the .csv file
+    '''
+
+    # Setting up the dataframe for ingestion
+    data: pd = pd.read_csv(MAIN_FOLDER / path_and_file, encoding = 'UTF-8')
+
+    # Returns the dataframe
+    return data
+
+def csv_from_df(file_to_write:pd, path_and_file: str, separation_type: str = '\t', encoding_type: str = 'UTF-8'):
+    '''
+    Takes a provided dataframe and saves it as a csv file with the provided name and directory
+
+    Parameters:
+        file_to_write(pandas): The dataframe to be saved as a csv file
+        path_and_file(string): Location and name details for the csv file to be saved
+        separation_type(string): The separator to use on the csv file, defaults to '\t'
+        encoding_type(string): The encoding to use on th csv file, defaults to 'UTF-8'
+    '''
+
+    # Writes the provide dataframe to a file
+    file_to_write.to_csv(MAIN_FOLDER / path_and_file, sep = separation_type, encoding = encoding_type)
+
+# Script run control
+if __name__ == "__main__":
+    pass
--- src/part1-etl.py
+++ src/part1-etl.py
@@ -2,13 +2,44 @@
 PART 1: ETL the two datasets and save each in `data/` as .csv's
 '''
 
+# Standard library imports
+from pathlib import Path
+
+# Third party imports
 import pandas as pd
 
-pred_universe_raw = pd.read_csv('https://www.dropbox.com/scl/fi/69syqjo6pfrt9123rubio/universe_lab6.feather?rlkey=h2gt4o6z9r5649wo6h6ud6dce&dl=1')
-arrest_events_raw = pd.read_csv('https://www.dropbox.com/scl/fi/wv9kthwbj4ahzli3edrd7/arrest_events_lab6.feather?rlkey=mhxozpazqjgmo6qqahc2vd0xp&dl=1')
-pred_universe_raw['arrest_date_univ'] = pd.to_datetime(pred_universe_raw.filing_date)
-arrest_events_raw['arrest_date_event'] = pd.to_datetime(arrest_events_raw.filing_date)
-pred_universe_raw.drop(columns=['filing_date'], inplace=True)
-arrest_events_raw.drop(columns=['filing_date'], inplace=True)
+# Local application imports
+from file_manager import csv_from_df
 
-# Save both data frames to `data/` -> 'pred_universe_raw.csv', 'arrest_events_raw.csv'
+# Setting constants
+MAIN_FOLDER: Path = Path(__file__).absolute().parent
+DATA_PATH: str = '../data/'
+
+# The etl function will extract the data from the url's, transform the data with the provided
+# instructions, and load the data into csv files in the ./data folder
+def etl():
+    '''
+    etl function performs the extract, transform, and load work necessary to proceed through the problem
+    '''
+
+    # Extract dataframes from the provided data sources
+    pred_universe_raw: pd = pd.read_csv('https://www.dropbox.com/scl/fi/69syqjo6pfrt9123rubio/universe_lab6.feather?rlkey=h2gt4o6z9r5649wo6h6ud6dce&dl=1')
+    arrest_events_raw: pd = pd.read_csv('https://www.dropbox.com/scl/fi/wv9kthwbj4ahzli3edrd7/arrest_events_lab6.feather?rlkey=mhxozpazqjgmo6qqahc2vd0xp&dl=1')
+
+    # Transform data within the dataframes to allow for future analysis
+    pred_universe_raw['arrest_date_univ'] = pd.to_datetime(pred_universe_raw.filing_date)
+    arrest_events_raw['arrest_date_event'] = pd.to_datetime(arrest_events_raw.filing_date)
+    pred_universe_raw.drop(columns=['filing_date'], inplace=True)
+    arrest_events_raw.drop(columns=['filing_date'], inplace=True)
+
+    # Save both data frames to `data/` -> 'pred_universe_raw.csv', 'arrest_events_raw.csv'
+    pred_file_name: str = 'pred_univers_raw.csv'
+    arrest_file_name: str = 'arrest_events_raw.csv'
+    
+    csv_from_df(pred_universe_raw, DATA_PATH + pred_file_name)
+    csv_from_df(arrest_events_raw, DATA_PATH + arrest_file_name)
+
+
+# Script run control
+if __name__ == "__main__":
+    etl()
